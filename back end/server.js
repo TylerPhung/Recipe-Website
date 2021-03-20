7ad7e823f0207f61ad9af676041dbf9331413c05
Tyler Phung <31814813+TylerPhung@users.noreply.github.com>
2021-03-19 22:54:26 -0700

    Added logic for accounts and searching

--- back end/server.js
+++ back end/server.js
@@ -2,24 +2,96 @@ const http = require("http")
 const fs = require("fs");
 
 let users = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
+let recipes = JSON.parse(fs.readFileSync('./recipes.json', 'utf8'));
 
 const server = http.createServer((req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Headers', '*');
 	
-	req.on('data', chunk => {
-		obj = JSON.parse(chunk);
+	switch(req.url) {
+		case "/new_account":
+			req.on('data', data => {
+				let obj = JSON.parse(data);
+				
+				if(obj.username in users) {
+					res.statusCode = 409;
+				}
+				else {
+					users[obj.username] = {pw: obj.password};
+					fs.writeFileSync('./users.json', JSON.stringify(users));
+				}
+				
+				res.end();
+			});
+			break;
 		
-		if(obj.username in users) {
-			res.statusCode = 409;
-		}
-		else {
-			users[obj.username] = {pw: obj.password};
-			fs.writeFileSync('./users.json', JSON.stringify(users));
-		}
 		
-		res.end();
-	});
+		
+		case "/sign_in":
+			req.on('data', data => {
+				let obj = JSON.parse(data);
+				
+				if(obj.username in users) {
+					if(obj.password !== users[obj.username].pw) {
+						res.statusCode = 409;
+					}
+				}
+				else {
+					res.statusCode = 409;
+				}
+				
+				res.end();
+			});
+			break;
+		
+		
+		
+		case "/recipes":
+			req.on('data', data => {
+				let obj = JSON.parse(data);
+				
+				recipes.push(obj);
+				fs.writeFileSync('./recipes.json', JSON.stringify(recipes));
+				
+				res.end();
+			});
+			break;
+		
+		
+		
+		case "/find_recipes":
+			req.on('data', data => {
+				let obj = JSON.parse(data);
+				let terms = (obj.terms ? obj.terms.split(" ") : []);
+				let payload = [];
+				
+				for(const i in recipes) {
+					let recipe = recipes[i];
+					
+					if(recipe.type === obj.type) {
+						payload.push(recipe);
+					}
+					else {
+						for(const j in terms) {
+							let term = terms[j];
+							
+							if(recipe.ingredients.includes(term)) {
+								payload.push(recipe);
+								break;
+							}
+						}
+					}
+				}
+				
+				res.end(JSON.stringify(payload));
+			});
+			break;
+		
+		
+		
+		default:
+			break;
+	}
 	
 	req.on('end', () => {
 		res.end();
--- front end/src/index.js
+++ front end/src/index.js
@@ -5,13 +5,15 @@ import http from 'http';
 
 
 
+/*
 const mealEnum = {
-	BREAKFAST: "BREAKFAST",
-	LUNCH: "LUNCH",
-	DINNER: "DINNER",
-	DESSERT: "DESSERT",
-	DRINK: "DRINK",
+	BREAKFAST: "breakfast",
+	LUNCH: "lunch",
+	DINNER: "dinner",
+	DESSERT: "dessert",
+	DRINK: "drink",
 }
+*/
 
 
 
@@ -34,18 +36,33 @@ class SignIn extends React.Component {
 class RecipeSummary extends React.Component {
 	render() {
 		return (
-			<span className="recipe_summary">
-				<img src={"./photos/" + this.props.photo} alt=""></img>
-				<div>
-					<h3>{
-						"\u2605".repeat(this.props.rating)
-						+ "\u2606".repeat(5 - this.props.rating)
-						+ " " + this.props.name
-					}</h3>
-					<p>{this.props.desc}</p>
-				</div>
-			</span>
+			<div className="recipe_summary">
+				<h3 onClick={this.props.onClick}>{
+					"\u2605".repeat(this.props.rating)
+					+ "\u2606".repeat(5 - this.props.rating)
+					+ " " + this.props.name
+					+ " - " + this.props.author
+				}</h3>
+				<p>{this.props.desc}</p>
+			</div>
 		);
+	}
+}
+
+
+
+class RecipeListing extends React.Component {
+	render() {
+		return <>
+			<h2>{this.props.name + " (By " + this.props.author + ")"}</h2>
+			<p>{this.props.desc}</p>
+			<br />
+			<h2>Ingredients</h2>
+			<p>{this.props.ingredients}</p>
+			<br />
+			<h2>Instructions</h2>
+			<p>{this.props.instructions}</p>
+		</>;
 	}
 }
 
@@ -54,19 +71,28 @@ class RecipeSummary extends React.Component {
 class CreateRecipe extends React.Component {
 	render() {
 		return <form>
-			<label htmlFor="name">Name of recipe: </label>
-			<input type="text" name="name" />
+			<label>Name of recipe: </label>
+			<input type="text" id="name" />
 			<br />
-			<label htmlFor="desc">Brief description: </label>
-			<input type="text" name="desc" />
+			<label>Brief description: </label>
+			<input type="text" id="desc" />
 			<br />
-			<label htmlFor="ingredients">Ingredients: </label>
+			<label>Meal type:</label>
+			<select id="type">
+				<option value="breakfast">Breakfast</option>
+				<option value="lunch">Lunch</option>
+				<option value="dinner">Dinner</option>
+				<option value="dessert">Dessert</option>
+				<option value="drink">Drink</option>
+			</select>
 			<br />
-			<textarea name="ingredients" cols="50" rows="5"></textarea>
+			<label>Ingredients: </label>
 			<br />
-			<label htmlFor="instructions">Instructions: </label>
+			<textarea id="ingredients" cols="50" rows="5"></textarea>
 			<br />
-			<textarea name="instructions" cols="50" rows="5"></textarea>
+			<label>Instructions: </label>
+			<br />
+			<textarea id="instructions" cols="50" rows="5"></textarea>
 		</form>;
 	}
 }
@@ -79,6 +105,8 @@ class Page extends React.Component {
 		this.state = {
 			mode: "view",
 			username: "",
+			results: [],
+			focus_recipe: null,
 		};
 	}
 	
@@ -86,19 +114,54 @@ class Page extends React.Component {
 		switch(this.state.mode) {
 			case "sign_in":
 				return <>
+					<button onClick={() => this.set_mode("view")}>Back</button>
 					<SignIn />
-					<button onClick={() => this.submit_form()}>Sign in</button>
+					<button onClick={() => this.sign_in()}>Sign in</button>
 					<button onClick={() => this.create_account()}>Create Account</button>
 				</>;
 			case "add":
 				return <>
+					<button onClick={() =>this.set_mode("view")}>Back</button>
 					<CreateRecipe />
-					<button onClick={() => this.submit_form()}>Submit</button>
+					<button onClick={() => this.publish_recipe()}>Submit</button>
 				</>;
+				
+			case "results":
+				return <>
+					<button onClick={() =>this.set_mode("view")}>Back</button>
+					{this.state.results.map((recipe, idx) =>
+						<RecipeSummary
+							rating="5"
+							name={recipe.name}
+							author={recipe.author}
+							desc={recipe.desc}
+							onClick={() => this.show_full_recipe(idx)}
+						/>
+					)}
+					{this.state.results.length === 0 ?
+						<p>No results</p> : null
+					}
+				</>;
+			
+			case "show_recipe":
+				return <>
+					<button onClick={() => this.set_mode("results")}>Back</button>
+					<RecipeListing
+						name={this.state.focus_recipe.name}
+						author={this.state.focus_recipe.author}
+						desc={this.state.focus_recipe.desc}
+						type={this.state.focus_recipe.type}
+						ingredients={this.state.focus_recipe.ingredients}
+						instructions={this.state.focus_recipe.instructions}
+					/>
+				</>
+			
 			default: // view
 				return <>
 					<span>
-						<button onClick={() => this.set_mode("add")}>Create recipe</button>
+						{this.state.username !== "" ?
+							<button onClick={() => this.set_mode("add")}>Create recipe</button> : null
+						}
 						<button onClick={() => 
 							this.state.username === "" ?
 								this.set_mode("sign_in") :
@@ -111,7 +174,25 @@ class Page extends React.Component {
 						}
 					</span>
 					
-					<RecipeSummary rating="4" name="PB&J" desc="White bread with peanut butter and grape jelly" photo="pb_j.jpg"/>
+					
+					<div>
+						<label>Search by ingredients:</label>
+						<br />
+						<input type="text" id="terms" />
+						<button onClick={() => this.search_by_ingredients()}>Search</button>
+						<br />
+						<label>OR</label>
+						<br />
+						<label>Search by meal type: </label>
+						<select id="type">
+							<option value="breakfast">Breakfast</option>
+							<option value="lunch">Lunch</option>
+							<option value="dinner">Dinner</option>
+							<option value="dessert">Dessert</option>
+							<option value="drink">Drink</option>
+						</select>
+						<button onClick={() => this.search_by_type()}>Search</button>
+					</div>
 				</>;
 		}
 	}
@@ -175,8 +256,153 @@ class Page extends React.Component {
 		.end(body);
 	}
 	
+	sign_in() {
+		let username = document.getElementById('username').value;
+		let password = document.getElementById('password').value;
+		
+		
+		let body = JSON.stringify({
+			username: username,
+			password: password,
+		});
+		let options = {
+			hostname: "localhost",
+			port: 5000,
+			path: "/sign_in",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Content-Length": Buffer.byteLength(body)
+			}
+		};
+		
+		
+		
+		http.request(options, res => {
+			res.on('data', () => {});
+			
+			res.on('end', () => {
+				if(res.statusCode === 200){
+					this.submit_form();
+				}
+				else {
+					document.getElementById('banner').innerHTML = "-- Invalid credentials, try again";
+					document.getElementById('password').value = "";
+				}
+			});
+		})
+		.on("error", console.error)
+		.end(body);
+	}
+	
 	sign_out() {
 		this.setState({username: ""});
+	}
+	
+	publish_recipe() {
+		let body = JSON.stringify({
+			author: this.state.username,
+			name: document.getElementById('name').value,
+			desc: document.getElementById('desc').value,
+			type: document.getElementById('type').value,
+			ingredients: document.getElementById('ingredients').value,
+			instructions: document.getElementById('instructions').value,
+		});
+		let options = {
+			hostname: "localhost",
+			port: 5000,
+			path: "/recipes",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Content-Length": Buffer.byteLength(body)
+			}
+		};
+		
+		
+		
+		http.request(options, res => {
+			res.on('data', () => {});
+			
+			res.on('end', () => {
+				this.submit_form();
+			});
+		})
+		.on("error", console.error)
+		.end(body);
+	}
+	
+	search_by_ingredients() {
+		let body = JSON.stringify({
+			terms: document.getElementById('terms').value,
+			type: null,
+		});
+		let options = {
+			hostname: "localhost",
+			port: 5000,
+			path: "/find_recipes",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Content-Length": Buffer.byteLength(body)
+			}
+		};
+		
+		
+		
+		http.request(options, res => {
+			res.on('data', data => {
+				this.setState({
+					results: JSON.parse(data)
+				});
+			});
+			
+			res.on('end', () => {
+				this.set_mode("results");
+			});
+		})
+		.on("error", console.error)
+		.end(body);
+	}
+	
+	search_by_type() {
+		let body = JSON.stringify({
+			terms: null,
+			type: document.getElementById('type').value,
+		});
+		let options = {
+			hostname: "localhost",
+			port: 5000,
+			path: "/find_recipes",
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Content-Length": Buffer.byteLength(body)
+			}
+		};
+		
+		
+		
+		http.request(options, res => {
+			res.on('data', data => {
+				this.setState({
+					results: JSON.parse(data)
+				});
+			});
+			
+			res.on('end', () => {
+				this.set_mode("results");
+			});
+		})
+		.on("error", console.error)
+		.end(body);
+	}
+	
+	show_full_recipe(num) {
+		this.setState({
+			mode: "show_recipe",
+			focus_recipe: this.state.results[num],
+		});
 	}
 }
 
